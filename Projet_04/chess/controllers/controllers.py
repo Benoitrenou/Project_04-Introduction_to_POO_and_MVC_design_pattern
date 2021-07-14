63c935cd5b5b2e9ea1d33d53eeaaea96bdd5507d
Benoitrenou <benoit.renou@gmail.com>
2021-07-14 08:19:58 -0300

    Ajout des premiers controllers et views

--- Projet_04/chess/controllers/controllers.py
+++ Projet_04/chess/controllers/controllers.py
@@ -1,12 +1,53 @@
-class Controller:
+from models.player import Player
+# from models.tournament import Tournament
+# from models.match import Match
+from utils.menus import Menu
+from views.view import *
+from tinydb import TinyDB, Query
+
+class ApplicationController:
 
 	def __init__(self):
-		pass
+		self.controller = None
 
-	def create_tournament(self):
-		pass
+	def start(self):
+		self.controller = HomeMenuController()
+		while self.controller:
+			self.controller = self.controller()
 
-	def create_player(self):
-		pass
+class HomeMenuController:
 
+	def __init__(self):
+		self.menu = Menu()
+		self.view = HomeMenuView(self.menu)
+
+	def __call__(self):
+		self.menu.add("auto", "Créer un joueur", CreatePlayerController)
+		self.menu.add("auto", "Créer un tournoi", CreateTournamentController)
+		self.menu.add("q", "Quitter l'application", EndScreenController)
+		user_choice = self.view.get_user_choice()
+		return user_choice.handler
+
+class CreatePlayerController:
+
+	def __init__(self):
+		self.view = CreatePlayerView()
+
+	def __call__(self):
+		new_player_attributes = self.view.get_player_informations()
+		new_player = Player(*new_player_attributes)
+		new_player.player_manager.save()
+		print (new_player)
+
+class CreateTournamentController:
 	
+	def __call__(self):
+		print ("Contrôleur de création de tournoi")
+
+class CreateReportController:
+	pass
+
+class EndScreenController:
+
+	def __call__(self):
+		print ("Quitter l'application")
--- Projet_04/chess/views/view.py
+++ Projet_04/chess/views/view.py
@@ -1,3 +1,26 @@
-class View:
+from models.player import Player
 
-	
+class HomeMenuView:
+
+	def __init__(self, menu):
+		self.menu = menu
+
+	def _display_menu(self):
+		for key, entry in self.menu.items():
+			print (f'{key} : {entry.option}')
+
+	def get_user_choice(self):
+		while True:
+			self._display_menu()
+			choice = input(">>")
+			if choice in self.menu:
+				return self.menu[choice]
+
+class CreatePlayerView:
+
+	def get_player_informations(self):
+		while True:
+			new_player_attributes = []
+			for item in Player.list_attributes():
+				new_player_attributes.append(input(f"{item} ?"))
+			return new_player_attributes
